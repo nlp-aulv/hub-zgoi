0c4957e43cf819f2c284cc342ba89a3251745e04
klm-yym <1724841586@qq.com>
2025-09-12 00:49:52 +0800

    Create 作业1.py

---
+++ 孔令萌/week05/作业1.py
@@ -0,0 +1,17 @@
+import openai
+
+client = openai.OpenAI(
+    api_key="sk-1a30fafa1b7f4f9f8e0deae1fbaf46f0",
+    base_url="https://api.deepseek.com/v1"
+)
+
+# 调用DeepSeek模型
+for resp in client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "user", "content": "证明费马大定理"}
+    ],
+    stream=True
+):
+    if resp.choices and resp.choices[0].delta.content:
+        print(resp.choices[0].delta.content, end="", flush=True)
