d3d5d7a35c8d6173f8f2133c1f0324758bb243ab
AvaXLY <38498561+AvaXLY@users.noreply.github.com>
2025-09-11 23:49:58 +0800

    Update deepseek_api.py

--- 徐凌云/week05/deepseek_api.py
+++ 徐凌云/week05/deepseek_api.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-d68c0c8e1e10496db0b1c1b8b4a1d172", base_url="https://api.deepseek.com/v1")
+client = OpenAI(api_key="sk-d68c0***********************d172", base_url="https://api.deepseek.com/v1")
 
 response = client.chat.completions.create(
     model="deepseek-chat",
