c18093fce4f3d144042fd58534cf99d5e31ab2d7
nlp-aulv <194391685@qq.com>
2025-09-12 16:42:10 +0800

    Update task01.py

--- 沈小琴/week05/task01.py
+++ 沈小琴/week05/task01.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-50f13f999dd34865917a6b5c36f9707e", base_url="https://api.deepseek.com")
+client = OpenAI(api_key="sk-50f13f999******6b5c36f9707e", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
     model="deepseek-chat",
